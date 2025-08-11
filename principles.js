cf452577614a811f4e78237497be67bd039dcf44
Kalidass <kalidass.k021@gmail.com>
2025-08-11 12:55:05 +0530

    initial commit

---
+++ principles.js
@@ -0,0 +1,9 @@
+// solid principles
+
+/* 
+    * S => Single Responsibility Principle (SRP)
+    * O => Open/ Closed Principle (OCP)
+    * L => Liskovv Substitution Principle (LSP)
+    * I => Interface Segregation Principle (ISP)
+    * D => Depentency Inversion Principle (DIP)
+*/
