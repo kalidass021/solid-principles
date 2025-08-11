00ebc771be4149b6ec76fec7d409eff68f4925fc
Kalidass <kalidass.k021@gmail.com>
2025-08-11 13:34:16 +0530

    single responsibility principle

---
+++ 1.singleResponsibilityPrinciple.js
@@ -0,0 +1,27 @@
+// a function should have only one responsibility
+
+// wrong
+
+const createAndValidateUser = (name, password, email) => {
+    // validate the data
+    const isFormValid = testForm(name, password, email);
+
+    if (!isFormValid) return;
+
+    // inserting the user data in the database
+    User.Create(name, password, email);
+}
+
+// correct way
+
+const validateRequest = (req) => {
+    const isFormValid = testForm(name, password, email);
+
+    if (!isFormValid) return;
+
+    // if form is valid
+    createUser(req);
+
+    // create user function
+    const createUser = (req) => User.Create(req.name, req.password, req.email);
+}
