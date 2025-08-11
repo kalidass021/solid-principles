e9904a0be9f57d698dfe86ab05d85824fa430ff5
Kalidass <kalidass.k021@gmail.com>
2025-08-11 13:36:26 +0530

    add srp notes

--- 0.principles.js
+++ 0.principles.js
@@ -2,7 +2,12 @@
 
 /* 
     * S => Single Responsibility Principle (SRP)
+
+        * A funciton should have only one responsibility
+        
     * O => Open/ Closed Principle (OCP)
+
+        * 
     * L => Liskov Substitution Principle (LSP)
     * I => Interface Segregation Principle (ISP)
     * D => Depentency Inversion Principle (DIP)
